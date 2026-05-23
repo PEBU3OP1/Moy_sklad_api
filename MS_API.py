aa548c0a53ee400196aa0b7bc8ad52731176e169
PEBU3OP1 <sevakuzya@mail.ru>
2026-05-23 18:59:15 +0300

    home_1.0

--- MS_API.py
+++ MS_API.py
@@ -3,23 +3,44 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-token = os.getenv('TOKEN_MS')
-print(token)
-exit()
-url = 'https://api.moysklad.ru/api/remap/1.2/entity/assortment'
-goods_url = 'url = "https://api.moysklad.ru/api/remap/1.2/entity/product'
+class MS_Ap:
+    def __init__(self):
+        load_dotenv()
+        self.token = os.getenv('TOKEN_MS')
+        self.header = {
+            'Authorization': f'Bearer {self.token}',
+            "Content-Type": "application/json"
+        }
 
-header = {
-    'Authorization': f'Bearer {token}',
-    "Content-Type": "application/json"
-}
-params = {
-    'filter': 'name = Диск тормозной 330 мм'
-}
+        #########   URLS  ###########
+        self.assortment_url = 'https://api.moysklad.ru/api/remap/1.2/entity/assortment'
+        self.goods_url = 'url = "https://api.moysklad.ru/api/remap/1.2/entity/product'
+        self.warehouse_url = 'https://api.moysklad.ru/api/remap/1.2/entity/store'
 
-response = requests.get(url = url, headers= header, params=params)
+    def get_info_by_api(self, url, params=None):
+        response = requests.get(url=url, headers = self.header, params=params)
+        if response.status_code == 200:
+            result = response.json()
+            print(json.dumps(result, indent=4, ensure_ascii=False))
+            return result
+        else:
+            print(response.status_code)
+    def get_goods(self):
 
-if response.status_code == 200:
-    goods = response.json()
-    print(json.dumps(goods, indent=4, ensure_ascii=False))
+        params = {
+            'filter': 'name = Диск тормозной 330 мм'
+        }
+
+        response = requests.get(url = self.assortment_url, headers= self.header, params=params)
+
+        if response.status_code == 200:
+            goods = response.json()
+            print(json.dumps(goods, indent=4, ensure_ascii=False))
+
+
+    def get_warehouses(self):
+        for wrhs_name in self.get_info_by_api(self.warehouse_url)['rows']:
+
+
+
+MS_Ap().get_warehouses()
